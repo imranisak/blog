05b220cf999a7881ea655c9b131d699ebf6844be
Imran Isak <info@imranisak.com>
2021-08-19 14:26:38 +0200

    added dropdown for categories

--- routes/web.php
+++ routes/web.php
@@ -22,24 +22,29 @@ use function GuzzleHttp\Promise\all;
 
 Route::get('/', function () {
     return view('posts', [
-        'posts'=>Post::latest()->with('category', 'author')->get()
+        'posts'=>Post::latest()->with('category', 'author')->get(),
+        'categories'=>Category::all()
     ]);
 });
 
 Route::get('posts/{post:slug}', function (Post $post){
     return view('post', [
-        'post'=> $post
+        'post'=> $post,
+        'categories'=>Category::all()
     ]);
 });
 
 Route::get('categories/{category:slug}', function(Category $category){
     return view('posts', [
-        'posts'=>$category->posts
+        'posts'=>$category->posts,
+        'currentCategory'=>$category,
+        'categories'=>Category::all()
     ]);
 });
 
 Route::get('authors/{author:username}', function(User $author){
     return view('posts', [
-        'posts'=>$author->posts
+        'posts'=>$author->posts,
+        'categories'=>Category::all()
     ]);
 });
