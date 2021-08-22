38ec88bf4b3c1ba0be77c761f4b44cccff895604
Imran Isak <info@imranisak.com>
2021-08-22 17:41:00 +0200

    Fixed search

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,10 +16,10 @@ class  DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $user=User::factory()->create();
-        Post::factory(15)->create([
-            'user_id'=>$user->id
-        ]);
+        $user = User::factory()->create();
 
+        Post::factory(15)->create([
+            'user_id' => $user->id
+        ]);
     }
 }
