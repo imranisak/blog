747401331f6c1da43df8a4168dbe6a7db53e77cc
Imran Isak <info@imranisak.com>
2021-09-09 11:57:50 +0200

    Update layout.blade.php

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,6 +7,11 @@
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
+<style>
+    html{
+        scroll-behavior: smooth;
+    }
+</style>
 <body style="font-family: Open Sans, sans-serif">
 <section class="px-6 py-8">
     <nav class="md:flex md:justify-between md:items-center">
@@ -27,7 +32,7 @@
                 <a href="/register" class="text-xs font-bold uppercase">Register</a>
                 <a href="/login" class="ml-6 text-xs font-bold uppercase">Log in</a>
             @endauth
-            <a href="#" class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
+            <a href="#newsletter" class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
                 Subscribe for Updates
             </a>
         </div>
@@ -38,7 +43,7 @@
         <h5 class="text-3xl">Stay in touch with the latest posts</h5>
         <p class="text-sm mt-3">Promise to keep the inbox clean. No bugs.</p>
 
-        <div class="mt-10">
+        <div class="mt-10" id="newsletter">
             <div class="relative inline-block mx-auto lg:bg-gray-200 rounded-full">
 
                 <form method="POST" action="#" class="lg:flex text-sm">
